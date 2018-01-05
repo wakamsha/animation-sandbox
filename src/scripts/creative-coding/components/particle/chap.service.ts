2bf64d7c165d27ec5f0199f751e51509210d6080
wakamsha <wakamsha@gmail.com>
2018-01-05 22:00:43 +0900

    feat: TV Noise

--- src/scripts/creative-coding/components/particle/chap.service.ts
+++ src/scripts/creative-coding/components/particle/chap.service.ts
@@ -1,12 +1,14 @@
 import {Chapter} from '../../../declares/interface';
 import {NodeGardenComponent} from './components/node-garden.component';
 import {Injectable} from '@angular/core';
+import {TvNoiseComponent} from './components/tv-noise.component';
 
 export const chapter: Chapter = {
     directory: 'particle',
     title: 'Particle',
     routes: {
-        'node-garden': NodeGardenComponent
+        'node-garden' : NodeGardenComponent,
+        'TV-noise'       : TvNoiseComponent
     }
 };
 
---
+++ src/scripts/creative-coding/components/particle/components/tv-noise.component.ts
@@ -0,0 +1,49 @@
+import {Component} from '@angular/core';
+import {BaseComponent, baseOptions} from '../../base.component';
+import {Title} from '@angular/platform-browser';
+
+@Component(baseOptions)
+export class TvNoiseComponent extends BaseComponent {
+
+    private dt: ImageData;
+    private dd: Uint8ClampedArray;
+    private dl: number;
+
+    constructor(a: Title) {
+        super(a);
+        this.title = 'Noise';
+    }
+
+    ngOnInit() {
+        super.init();
+        this.setupData();
+    }
+
+    protected onEnterFrame() {
+        this.generateNoise();
+        this.ctx.fillStyle = this.ctx.createPattern(this.stage, 'repeat');
+        this.ctx.fillRect(0, 0, this.stageWidth >> 1, this.stageHeight >> 1);
+    }
+
+    protected onResized() {
+        this.setupData();
+    }
+
+    private setupData() {
+        this.dt = this.ctx.createImageData(this.stageWidth, this.stageHeight);
+        this.dd = this.dt.data;
+        this.dl = this.dt.width * this.dt.height;
+    }
+
+    private generateNoise() {
+        let p = 0;
+        for (let i = 0; i < this.dl; i++) {
+            const c = Math.floor(Math.random() * 256);
+            this.dd[p++] = c;
+            this.dd[p++] = c;
+            this.dd[p++] = c;
+            this.dd[p++] = 255;
+        }
+        this.ctx.putImageData(this.dt, 0, 0);
+    }
+}
